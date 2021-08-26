f03013ba6a58fa02288244c313e4dfe19168bf33
Andrei9507 <andreibirta95@gmail.com>
2021-08-26 22:10:06 +0300

    changes on new branch

--- server/models/project.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-const Schema = mongoose.Schema;
-
-const projectScheema = new Schema({
-    name: String,
-    description: String,
-});
-
-module.exports = mongoose.model('Project', projectScheema);
